997a7a15bd467f3ccf6b7fb76dd46fd35f447edf
Boselor <xiaoyong@boselor.com>
2021-01-30 18:27:07 +0800

    添加SFM功能

---
+++ Plugins/app/advanced/Video2Mat.h
@@ -0,0 +1,28 @@
+//
+// Created by xiaoyong on 2021/1/27.
+//
+
+#ifndef KUAFOO_VIDEO2MAT_H
+#define KUAFOO_VIDEO2MAT_H
+
+#include <opencv2/opencv.hpp>
+#include "Logger.h"
+#include "Utils.h"
+
+using namespace std;
+using namespace cv;
+
+class Video2Mat {
+private:
+    Utils* util;
+public:
+    struct Params{
+        string  file,folder,ext = "png";
+        int start = 1,stop = -1,skip = 0;
+        Logger* logger;
+    }params;
+    void run();
+};
+
+
+#endif //KUAFOO_VIDEO2MAT_H
